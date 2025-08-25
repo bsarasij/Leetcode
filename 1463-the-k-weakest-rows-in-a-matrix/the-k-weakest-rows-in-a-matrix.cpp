fed2036b6471bf831822a370ebdb771e4bc107b2
Sarasij Banerjee <sbaner74@asu.edu>
2025-08-24 17:56:04 -0700

    Time: 0 ms (100.00%) | Memory: 14.4 MB (38.43%) - LeetSync

---
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        std::unordered_map<int, int> map;
+        std::priority_queue<pair<int,int>> heap;
+        for (int row = 0; row < mat.size(); row++){
+            map[row] = std::accumulate(mat[row].begin(), mat[row].end(), 0);
+        }
+        for (auto& row: map){
+            heap.push({row.second, row.first});    
+            if (heap.size() > k){
+                heap.pop();
+            }
+        };
+        vector<int> weakRows;
+        while(!heap.empty()){
+            weakRows.push_back(heap.top().second);
+            heap.pop();
+        }
+        reverse(weakRows.begin(), weakRows.end());
+        return weakRows;
+    }
+};

5ea8828ada942c3dca5f4e1d0a60510b8c83461e
Sarasij Banerjee <sbaner74@asu.edu>
2025-09-12 20:39:50 -0700

    Time: 72 ms (93.12%) | Memory: 44.1 MB (91.73%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int inf = 1e4;
-        vector <int> d (n, inf);
-        d[k-1] = 0;
-        for (int i = 0; i < n-1; i++){
-            for (vector <int> edge : times){
-                int u = edge[0]-1;
-                int v = edge[1]-1;
-                int w = edge[2];
-                if (d[u] != inf && d[u] + w < d[v]){
-                    d[v] = d[u] + w;
-                }
-            }
+       using pr = pair<int, int>;
+        unordered_map<int, vector<pr>> adj;
+        for (vector<int>& time : times){
+            int first = time[0];
+            int second = time[1];
+            int delay = time[2];
+            adj[first].push_back({second, delay});
+            // adj[second].push_back({first, delay});
         }
-        int maxDelay = *max_element(d.begin(), d.end());
-        return (maxDelay == inf) ? -1 : maxDelay;    }
+        priority_queue<pr, vector<pr>, greater<pr>> heap;
+        vector<int> delay(n, INT_MAX);
+        delay[k-1] = 0;
+        heap.push({0,k});
+        while(!heap.empty()){
+            pr top = heap.top();
+            heap.pop();
+            int curr_delay = top.first;
+            int source = top.second;
+            for (auto& [nbr, weight]: adj[source]){
+                if (weight + curr_delay < delay[nbr-1]){
+                    delay[nbr-1] = weight + curr_delay;
+                    heap.push({delay[nbr-1], nbr});
+                }
+            }  
+        }
+        int max_delay = *max_element(delay.begin(), delay.end());
+        for (int i : delay){
+            cout << i << endl;
+        }
+        
+        return (max_delay!= INT_MAX) ? max_delay : -1;
+    }
 };

e0fdeba3d8793f79209276193a8309eae99c040f
Sarasij Banerjee <sbaner74@asu.edu>
2025-11-15 12:32:48 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,24 +1,32 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        row, col = len(matrix), len(matrix[0])
-        left = 0
-        up = 0
-        right = col - 1
-        down = row - 1
-        results = []
-        while len(results) < row*col:
+        if not matrix or not matrix[0]:
+            return []
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        arr = [0 for i in range(m*n)]
+        up = left = 0
+        down = m - 1
+        right = n - 1
+        idx = 0
+        while left <= right and up <= down:
             for j in range(left, right + 1):
-                results.append(matrix[up][j])
+                arr[idx] = matrix[up][j]
+                idx += 1
             for i in range(up + 1, down + 1):
-                results.append(matrix[i][right])
-            if up != down: 
+                arr[idx] = matrix[i][right]
+                idx += 1
+            if up != down:
                 for j in range(right - 1, left - 1, -1):
-                    results.append(matrix[down][j])
-            if right != left:
+                    arr[idx] = matrix[down][j]
+                    idx += 1
+            if left != right:
                 for i in range(down - 1, up, -1):
-                    results.append(matrix[i][left])
+                    arr[idx] = matrix[i][left]
+                    idx += 1
             up += 1
+            down -= 1
             left += 1
             right -= 1
-            down -= 1
-        return results
+        return arr

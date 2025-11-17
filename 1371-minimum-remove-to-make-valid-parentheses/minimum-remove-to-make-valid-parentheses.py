306b647c3981e36368373c1c413051a9245cece9
Sarasij Banerjee <sbaner74@asu.edu>
2025-11-17 00:19:45 -0700

    Time: 31 ms (82.28%) | Memory: 19 MB (80.22%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,20 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        s_list = list(s)
+        stack_op = []
         count = 0
-        mod_s = []
-        for c in s:
+        for i, c in enumerate(s_list):
             if c == '(':
+                stack_op.append(i)
                 count += 1
-                mod_s.append(c)
-            elif c == ')' and count >  0:
-                count -= 1
-                mod_s.append(c)
-            elif c != ')':
-                mod_s.append(c)
-        filtered_s = []
-        for c in mod_s[::-1]:
-            if c == '(' and count > 0:
-                count -= 1
-            else:
-                filtered_s.append(c)
-        return ''.join(filtered_s[::-1])
+            elif c == ')':
+                if count > 0:
+                    stack_op.pop()
+                    count -= 1
+                else:
+                    s_list[i] = ""
+        while stack_op:
+            s_list[stack_op.pop()] = ""
+        return "".join(s_list)
